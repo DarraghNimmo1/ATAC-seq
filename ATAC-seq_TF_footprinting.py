6709eabe1bd5f8e9ea6ff5a1eff7a2e4dce904b8
Darragh Nimmo <71436230+DarraghNimmo1@users.noreply.github.com>
2021-03-21 17:11:59 +0000

    Update ATAC-seq_TF_footprinting.py

--- ATAC-seq_TF_footprinting.py
+++ ATAC-seq_TF_footprinting.py
@@ -1,4 +1,3 @@
-
 ####################################################
 ## Darragh Nimmo
 ## Trinity College Dublin
@@ -12,27 +11,29 @@
 
 import os
 
-configfile: 'config.yaml'
+import functools
 
-BAMS = config['bams']
+configfile: 'config.yaml'
 
 CELL_TYPE = config['cell_type']
 
 REPLICATE = config['replicate']
 
-GENOME = config['genome']
+IN_BAM = config['in_bam']
 
 BLACK_LIST = config['black_list']
 
+PICARD_JAR = config['picard_jar']
+
+GENOME = config['genome']
+
 MOTIFS = config['motifs']
 
-
 directory_function = functools.partial(os.path.join, config['results'])
-IN_BAM = directory_function('In_Bam')
-OUT_BAM = directory_function('Out_Bam')
 PEAK_DIR  = directory_function('Peaks')
+OUT_BAM = directory_function('Out_Bam')
 ATAC_CORRECT = directory_function('ATAC_Correct')
-FOOT_PRINT_SCORE = directory_function('Foot_Print_Score')
+FOOT_PRINT_SCORE = directory_function('Foot_print_score')
 BIN_DETECT = directory_function('Bin_Detect')
 
 ###############################################################################################
@@ -40,17 +41,22 @@ BIN_DETECT = directory_function('Bin_Detect')
 ###############################################################################################
 
 rule all:
-        input:
-		expand(directory(os.path.join(BIN_DETECT, '/{cell_type}'), cell_type = CELL_TYPE))
-
-
-rule Peak_calling:
     input:
-        bam = expand(os.path.join(IN_BAM, '{cell_type}_rep_{replicate}.bam'), cell_type = CELL_TYPE, replicate = REPLICATE)
+        expand(os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_peaks.broadPeak'), cell_type = CELL_TYPE, replicate = REPLICATE),
+        expand(os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_preprocessed.broadPeak'), cell_type = CELL_TYPE, replicate = REPLICATE),
+        expand(os.path.join(OUT_BAM, '{cell_type}_merged.bam'), cell_type = CELL_TYPE),
+        expand(os.path.join(PEAK_DIR, '{cell_type}_merged.bed'), cell_type = CELL_TYPE),
+        expand(os.path.join(ATAC_CORRECT, '{cell_type}_corrected.bw'), cell_type = CELL_TYPE),
+        expand(os.path.join(FOOT_PRINT_SCORE, '{cell_type}_FootPrintScore.bw'), cell_type = CELL_TYPE),
+        expand(os.path.join(BIN_DETECT, '{cell_type}', 'bindetect_figures.pdf'), cell_type = CELL_TYPE)
+
+rule peak_calling:
+    input:
+        bam = os.path.join(IN_BAM, '{cell_type}_rep_{replicate}.bam')
     output:
-        BroadPeak = expand(os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}.broadPeak'), cell_type = CELL_TYPE, replicate = REPLICATE)
+        BroadPeak = os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_peaks.broadPeak')
     params:
-	name = expand('{cell_type}_rep_{replicate}', cell_type = CELL_TYPE, replicate = REPLICATE)
+        name = '{cell_type}_rep_{replicate}',
         dir = PEAK_DIR
     message:
         "Peak calling."
@@ -58,106 +64,101 @@ rule Peak_calling:
         """
         macs2 callpeak -t {input.bam} --nomodel --shift -100 --extsize 200 --broad -f BAM --name {params.name} -g hs --outdir {params.dir}
         """
-		     
 rule preprocess_peaks:
-	input:
-		BroadPeak = rules.peak_calling.output.BroadPeak,
-	output:
-		BroadPeak = expand(os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_preprocessed.broadPeak'), cell_type = CELL_TYPE, replicate = REPLICATE)
-	params:
-		BlackList = BLACK_LIST
-	message:
-		"Preprocessing peaks."
-	shell:
-		"""
-		bedtools subtract -a {input.BroadPeak} -b {params.BlackList} > {output.BroadPeak}
-		"""
-		     
-rule merge_bams:
         input:
-                rep1 = expand(os.path.join(IN_BAM, '{cell_type}_rep_one.bam'), cell_type = CELL_TYPE),
-                rep2 = expand(os.path.join(IN_BAM, '{cell_type}_rep_two.bam'), cell_type = CELL_TYPE)
+                BroadPeak = os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_peaks.broadPeak')
         output:
-                bam = expand(os.path.join(IN_BAM, '{cell_type}_merged.bam'), cell_type = CELL_TYPE),
-                index = expand(os.path.join(OUT_BAM, '{cell_type}_merged.bam.bai'), cell_type = CELL_TYPE)
+                BroadPeak = os.path.join(PEAK_DIR, '{cell_type}_rep_{replicate}_preprocessed.broadPeak')
+        params:
+                BlackList = BLACK_LIST
         message:
-            "Merging replicate bam files and indexing."
+                "Preprocessing peaks."
         shell:
-            """
-            java -jar PICARD_JAR MergeSamFiles I= {input.rep1} I= {input.rep2} O= {output.bam};
-            samtools index -@ 48 {output.bam}
-            """
+                """
+                bedtools subtract -a {input.BroadPeak} -b {params.BlackList} > {output.BroadPeak}
+                """
+
+rule merge_bams:
+    input:
+        rep1 = os.path.join(IN_BAM, '{cell_type}_rep_one.bam'),
+        rep2 = os.path.join(IN_BAM, '{cell_type}_rep_two.bam')
+    output:
+        bams = os.path.join(OUT_BAM, '{cell_type}_merged.bam'),
+        index = os.path.join(OUT_BAM, '{cell_type}_merged.bam.bai')
+    params:
+        jar = PICARD_JAR
+    message:
+        "Merging replicate bam files and indexing."
+    shell:
+        """
+        java -jar {params.jar} MergeSamFiles I= {input.rep1} I= {input.rep2} O= {output.bam};
+        samtools index -@ 48 {output.bam}
+        """
+
+
 
 rule merge_peaks:
-	input:
-		rep1 = expand(os.path.join(PEAK_DIR, '{cell_type}_rep_one_preprocessed.broadPeak'), cell_type = CELL_TYPE),
-		rep2 = expand(os.path.join(PEAK_DIR, '{cell_type}_rep_two_preprocessed.broadPeak'), cell_type = CELL_TYPE)
-	output:
-		peaks = expand(os.path.join(PEAK_DIR, '{cell_type}_merged.bed'), cell_type = CELL_TYPE)
-	message:
-		"Merging replicate peak files."
-	shell:
-		"""
-		cat {input.rep1} {input.rep2}| sort -k1,1 -k2,2n | bedtools merge -d 5 -c 4 -o distinct > {output.peaks}
-		"""
+    input:
+        rep1 = os.path.join(PEAK_DIR, '{cell_type}_rep_one_preprocessed.broadPeak'),
+        rep2 = os.path.join(PEAK_DIR, '{cell_type}_rep_two_preprocessed.broadPeak')
+    output:
+        peaks = os.path.join(PEAK_DIR, '{cell_type}_merged.bed')
+    message:
+        "Merging replicate peak files."
+    shell:
+        """
+        cat {input.rep1} {input.rep2}| sort -k1,1 -k2,2n | bedtools merge -d 5 -c 4 -o distinct > {output.peaks}
+        """
+
 rule ATAC_correct:
-	input:
-		peak = rules.merge_peaks.ouput.peaks,
-		bam = rules.merge_bams.output.peaks
-	output:
-		uncorrected = expand(os.path.join(ATAC_CORRECT, '{cell_type}_uncorrected.bw'), cell_type = CELL_TYPE),
-		bias = expand(os.path.join(ATAC_CORRECT, '{cell_type}_bias.bw'), cell_type = CELL_TYPE),	     
-		expected = expand(os.path.join(ATAC_CORRECT, '{cell_type}_expected.bw'), cell_type = CELL_TYPE),
-		corrected = expand(os.path.join(ATAC_CORRECT, '{cell_type}_corrected.bw'), cell_type = CELL_TYPE),	      
-		pdf = expand(os.path.join(ATAC_CORRECT, '{cell_type}_atacorrect.pdf'), cell_type = CELL_TYPE),
-		pickle = expand(os.path.join(ATAC_CORRECT, '{cell_type}_AtacBias.pickle'), cell_type = CELL_TYPE)
-	params:
-		genome = GENOME,
-		black_list = BLACK_LIST,
-		dir = ATAC_CORRECT,
-		name = expand('{cell_type}', cell_type = CELL_TYPE)
-	message:
-		"Correcting for Tn5 insertion bias and shifting reads to correct for Tn5 insertions."
-	shell:
-		"""
-		TOBIAS ATACorrect --bam {input.bam} --genome {params.genome} --peaks {input.peak} --blacklist {params.black_list} --outdir {params.dir} --prefix {params.name}  --cores 48
-		"""
-		
+    input:
+        peak = rules.merge_peaks.output.peaks,
+        bam = rules.merge_bams.output.bams
+    output:
+        uncorrected = os.path.join(ATAC_CORRECT, '{cell_type}_uncorrected.bw'),
+        bias = os.path.join(ATAC_CORRECT, '{cell_type}_bias.bw'),
+        expected = os.path.join(ATAC_CORRECT, '{cell_type}_expected.bw'),
+        corrected = os.path.join(ATAC_CORRECT, '{cell_type}_corrected.bw'),
+        pdf = os.path.join(ATAC_CORRECT, '{cell_type}_atacorrect.pdf'),
+        pickle = os.path.join(ATAC_CORRECT, '{cell_type}_AtacBias.pickle')
+    params:
+        genome = GENOME,
+        black_list = BLACK_LIST,
+        dir = ATAC_CORRECT,
+        name = '{cell_type}'
+    message:
+        "Correcting for Tn5 insertion bias and shifting reads to correct for Tn5 insertions."
+    shell:
+        """
+        TOBIAS ATACorrect --bam {input.bam} --genome {params.genome} --peaks {input.peak} --blacklist {params.black_list} --outdir {params.dir} --prefix {params.name}  --cores 48
+        """
+
 rule FootPrintScore:
-	input:
-		signal = rules.ATAC_correct.output.corrected,
-		region = rules.merge_peaks.output.peaks
-	output:
-		bigwig = expand(os.path.join(FOOT_PRINT_SCORE, '{cell_type}_FootPrintScore.bw'), cell_type = CELL_TYPE)
-	message:
-		"Calculating footprint score."
-	shell:
-		"""
-		TOBIAS ScoreBigwig --signal {input.signal} --regions {input.region} --output {output.bigwig} --cores 48 
-		"""	
-		
+    input:
+        signal = rules.ATAC_correct.output.corrected,
+        region = rules.merge_peaks.output.peaks
+    output:
+        bigwig = os.path.join(FOOT_PRINT_SCORE, '{cell_type}_FootPrintScore.bw')
+    message:
+        "Calculating footprint score."
+    shell:
+        """
+        TOBIAS ScoreBigwig --signal {input.signal} --regions {input.region} --output {output.bigwig} --cores 48
+        """
+
 rule BinDetect:
-	input:
-		signal = rules.FootPrintScore.output.bigwig,
-		region = rules.merge_peaks.output.peaks
-	output:
-		TF = expand(directory(os.path.join(BIN_DETECT, '/{cell_type}'), cell_type = CELL_TYPE))
-	params:
-		genome = GENOME
-		motifs = MOTIFS
-		dir = expand(os.path.join(BIN_DETECT, '/{cell_type}'), cell_type = CELL_TYPE)
-	message:
-		"Running bin detect on to make predictions on specific transcription factor binding at peaks"
-	shell:
-		"""
-		TOBIAS BINDetect --motifs {params.motifs} --signals {input.signal} --genome {params.genome} --peaks {input.region} --outdir {params.dir} --cores 48
-		"""
-
-
-		
-		
-		
-		
-		
-		
-		
+    input:
+        signal = rules.FootPrintScore.output.bigwig,
+        region = rules.merge_peaks.output.peaks
+    output:
+        TF = os.path.join(BIN_DETECT, '{cell_type}', 'bindetect_figures.pdf')
+    params:
+        genome = GENOME,
+        motifs = MOTIFS,
+        dir = os.path.join(BIN_DETECT, '{cell_type}')
+    message:
+        "Running bin detect on to make predictions on specific transcription factor binding at peaks"
+    shell:
+        """
+        TOBIAS BINDetect --motifs {params.motifs} --signals {input.signal} --genome {params.genome} --peaks {input.region} --outdir {params.dir} --cores 48
+        """
